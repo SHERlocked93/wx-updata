c1abad6f713fb9cf3a807725eb5753ce11dc90e0
SHERlocked93 <443033099@qq.com>
2020-07-21 23:53:53 +0800

    fix: 解决数组中嵌套数组时格式错误的问题

--- src/index.js
+++ src/index.js
@@ -11,32 +11,45 @@ function isObjectOrArray(tar) {
 }
 
 /**
+ * 处理数组
+ * @param value 当前值
+ * @param curPath 当前路径
+ * @param store 结果
+ */
+const handleArray = (value, curPath, store) => {
+    value.forEach((item, idx) => {        // forEach 会跳过数组空位
+        const arrPath = `${ curPath }[${ idx }]`  // 拼接数组路径
+        console.log({ item, idx, arrPath, curPath })
+        if (isObject(item)) {
+            objToPath(item, arrPath + '.', store)
+        } else if (isArray(item)) {
+            handleArray(item, arrPath, store)
+        } else {
+            store[arrPath] = item
+        }
+    })
+}
+
+/**
  * 对象转化为路径格式对象
  * @param obj 要转化的对象
  * @param prefix 路径前缀
  * @param store 结果
  */
-export const objToPath = (obj,
-                          prefix = '',
-                          store = {}) => {
+const objToPath = (obj,
+                   prefix = '',
+                   store = {}) => {
     for (const [key, value] of Object.entries(obj)) {
         const curPath = prefix === ''  // 当前路径
           ? key
-          : prefix.endsWith(']')
+          : prefix.endsWith('].')
             ? `${ prefix }${ key }`
             : `${ prefix }/${ key }`
 
         if (isObject(value)) {                    // 是对象
             objToPath(value, curPath, store)
         } else if (isArray(value)) {              // 是数组
-            value.forEach((item, idx) => {        // forEach 会跳过数组空位
-                const arrPath = `${ curPath }[${ idx }]`  // 拼接数组路径
-                if (isObjectOrArray(item)) {
-                    objToPath(item, arrPath, store)
-                } else {
-                    store[arrPath] = item
-                }
-            })
+            handleArray(value, curPath, store)
         } else {
             store[curPath] = value
         }
